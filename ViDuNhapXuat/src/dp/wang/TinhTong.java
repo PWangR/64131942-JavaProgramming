c4bdcadad92066a716fc68be2533eda2bb89a817
PWangR <124851875+PWangR@users.noreply.github.com>
2025-02-13 11:15:44 +0700

    Cập nhật ví dụ nhập
    Hoàn thiện chương trình tính tổng 2 số

--- ViDuNhapXuat/src/dp/wang/TinhTong.java
+++ ViDuNhapXuat/src/dp/wang/TinhTong.java
@@ -1,11 +1,24 @@
 package dp.wang;
+import java.util.Scanner;
 
 public class TinhTong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// In ra tên chương trình
+		System.out.println("Chương trình tính tổng 2 số");
+		//Tạo đối tượng nhập
+		Scanner nhap = new Scanner(System.in);
+		//Nhắc người dùng nhập 2 số
+		System.out.println("Nhập vào 2 số");
+		//Nhap A
+		double a= nhap.nextDouble();
+		//Nhap B
+		double b= nhap.nextDouble();
+		double tong = a + b;
+		System.out.print("Tổng là : ");
+		System.out.println(tong);
 		
-
 	}
 
 }
