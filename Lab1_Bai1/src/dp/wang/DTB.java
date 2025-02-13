117e3a104fe5fea583777bb080b3088a961cb14c
PWangR <124851875+PWangR@users.noreply.github.com>
2025-02-13 22:30:48 +0700

    Lab1_Bai1
    Hoàn thiện bài 1 Viết chương trình cho phép nhập họ và tên sinh viên, điểm trung bình từ bàn phím sau đó xuất ra màn hình.

--- Lab1_Bai1/src/dp/wang/DTB.java
+++ Lab1_Bai1/src/dp/wang/DTB.java
@@ -1,10 +1,22 @@
 package dp.wang;
+import java.util.Scanner;
+
 
 public class DTB {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner nhap = new Scanner(System.in);
+		System.out.print("Nhập vào tên sinh viên (vd: Nguyen Van A): ");
+		String tenSV = nhap.nextLine();
+		System.out.print("Nhập vào điểm trung bình (vd: 9,8): ");
+		double diemTB = nhap.nextDouble();
+		
+		nhap.nextLine(); // FIX LỖI: Đọc dòng trống sau nextFloat()
+		
+		System.out.println("Họ và tên: " + tenSV +"\nĐiểm trung bình: " + diemTB);
 
+		nhap.close(); // Đóng Scanner để tránh rò rỉ tài nguyên
 	}
 
 }
