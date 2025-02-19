aeffa791d18bf2e551c1ffd1a4c9ef9e12f8f598
PWangR <124851875+PWangR@users.noreply.github.com>
2025-02-19 21:42:40 +0700

    Bài 2 Lab2
    Viết chương trình cho phép giải phương trình bậc hai trong đó các hệ số a, b và c
    nhập từ bàn phím

---
+++ Lab2/src/lab_2/bai_2.java
@@ -0,0 +1,39 @@
+package lab_2;
+
+import java.util.Scanner;
+
+public class bai_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner nhap = new Scanner(System.in);
+		
+		System.out.print("Nhập vào a : ");
+		double a = nhap.nextDouble();
+		
+		System.out.print("Nhập vào b : ");
+		double b = nhap.nextDouble();
+		
+		System.out.print("Nhập vào c : ");
+		double c = nhap.nextDouble();
+		
+		double Delta = Math.pow(b,2) - 4*a*c;
+		
+		if(Delta < 0)
+		{
+			System.out.println("Phương trình vô nghiệm (Delta < 0)");
+		}
+		else if(Delta == 0)
+		{
+			System.out.printf("Phương trình có nghiệm kép : %.2f", -b/(2*a));
+		}
+		else
+		{
+			System.out.printf("Phương trình có 2 nghiệm phân biệt: x1 = %.2f ; x2 = %.2f", ((-b+Math.sqrt(Delta))/(2*a)), ((-b-Math.sqrt(Delta))/(2*a)) );
+		}
+			
+		
+		nhap.close();
+	}
+
+}
