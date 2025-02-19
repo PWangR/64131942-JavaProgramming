c1a9677f738719fdc66bdc399511f84a22384f75
PWangR <124851875+PWangR@users.noreply.github.com>
2025-02-19 21:33:35 +0700

    Bài 1 Lab2
    Viết chương trình cho phép giải phương trình bậc nhất trong đó các hệ số a và b
    nhập từ bàn phím

---
+++ Lab2/src/lab_2/bai_1.java
@@ -0,0 +1,33 @@
+package lab_2;
+import java.util.Scanner;
+
+public class bai_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner nhap = new Scanner(System.in);
+		
+		System.out.print("Nhập vào a : ");
+		double a = nhap.nextDouble();
+		
+		System.out.print("Nhập vào b : ");
+		double b = nhap.nextDouble();
+		
+		if(a == 0)
+		{
+			if(b == 0)
+			{
+				System.out.println("Phương trình có vô số nghiệm");
+			}
+			else
+			{
+				System.out.println("Phương trình vô nghiệm");
+			}
+		}
+		else
+		{
+			System.out.printf("Phương trình có nghiệm x = %.2f", -b/a );
+		}
+	}
+
+}
