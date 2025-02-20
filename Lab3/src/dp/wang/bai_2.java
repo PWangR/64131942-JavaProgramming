7993eef95e729ca408c24ee35382f9d16fe5813e
PWangR <124851875+PWangR@users.noreply.github.com>
2025-02-20 10:05:06 +0700

    Lab3 bài2
    code dang dở, lát code tiếp

--- Lab3/src/dp/wang/bai_2.java
+++ Lab3/src/dp/wang/bai_2.java
@@ -1,10 +1,19 @@
 package dp.wang;
+import java.util.Scanner;
+
 
 public class bai_2 {
+	
+	Scanner nhap = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public static void InBangCuuChuong(int n)
+	{
+		
 	}
 
 }
